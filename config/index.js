2b90f671c92e0b38f2e34dabffdffb745def062c
yinxiaobo <623303468@qq.com>
2019-11-15 17:07:10 +0800

    创建deploy脚本，修改publicPath

--- config/index.js
+++ config/index.js
@@ -105,7 +105,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/music/',
+    assetsPublicPath: '/vue-music/',
 
     /**
      * Source Maps
---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+
+# 生成静态文件
+npm run build
+
+# 源码推送到远程仓库 master 分支
+git add -A
+git commit -m 'deploy push master'
+git push -u github master
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名
+# cat>CNAME<<EOF
+# xxx.com
+# www.xxx.com
+# EOF
+
+# git 操作
+git init
+git add -A
+git commit -m 'deploy'
+
+# 发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:mx623303468/vue-music.git master:gh-pages
+
+cd -
